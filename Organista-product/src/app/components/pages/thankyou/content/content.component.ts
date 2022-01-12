b03e5a5644cc592a4334ac9b7689483354ba9fa2
Abdul <ahaseeb293@gmail.com>
2022-01-12 12:46:58 +0500

    CSS Changes

--- Organista-product/src/app/components/pages/thankyou/content/content.component.ts
+++ Organista-product/src/app/components/pages/thankyou/content/content.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-content',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+  storeID: string;
+  banner: any;
 
-  constructor() { }
+  constructor(private apiService: ApiService,
+    private httpClient: HttpClient,
+    private route: Router,
+    private activatedRoute: ActivatedRoute) { this.storeID = "McD" }
 
+     //Banner
+  getAssets(storeID){
+    return new Promise(resolve => {
+        // check count Item in Cart 
+        this.apiService.getStoreAssets(storeID).subscribe((res: any) => {
+            resolve(res.data)
+            let data = res.data;
+            this.banner = data.bannerUrl;
+            // this.assetsData = res.data;
+        }, error => {
+            // Swals.fire("Oops...", "Error : <small style='color: red; font-style: italic;'>" + error.error.message + "</small>", "error")
+        }) 
+        
+    });
+
+}
   ngOnInit(): void {
+    this.getAssets(this.storeID)
   }
 
 }
