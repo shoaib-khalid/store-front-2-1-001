3584977eaed70ecf1ce444e52a3f4e86c71f8237
imran-ks <imran.tariq@kalsym.com>
2022-02-01 17:37:14 +0500

    Remove hardcoding of domain name

--- src/app/store.service.ts
+++ src/app/store.service.ts
@@ -29,11 +29,11 @@ export class StoreService {
   }
 
   async parseStoreFromUrl() {
-    // let currBaseUrl = location.origin;
+    let currBaseUrl = location.origin;
 
     // For testing purposes
     // let currBaseUrl = "awan-tech.dev-pk2.symplified.ai";
-    let currBaseUrl = "mcd.dev-pk2.symplified.ai";
+    // let currBaseUrl = "mcd.dev-pk2.symplified.ai";
 
     const domainName = currBaseUrl.split('.')[0].replace(/^(https?:|)\/\//, '');
     const store: Store = await this.getStoreByDomainName(domainName);
